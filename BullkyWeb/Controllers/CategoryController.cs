d933e2809d4e1d8919cea98a7dcdefbeadc64e89
Mohammadfaizan <m.faizan.s2255@gmail.com>
2023-05-13 03:31:15 +0530

    Added: Update, Delete Action Methods and Partial View

--- BullkyWeb/Controllers/CategoryController.cs
+++ BullkyWeb/Controllers/CategoryController.cs
@@ -34,6 +34,7 @@ namespace BullkyWeb.Controllers
             {
                 _db.Categories.Add(obj);
                 _db.SaveChanges();
+                TempData["success"] = "Category created successfully";
                 return RedirectToAction("Index");
             }
            return View();
@@ -45,27 +46,65 @@ namespace BullkyWeb.Controllers
             {
                 return NotFound();
             }
-            return View();
+            Category? categoryFromDb = _db.Categories.Find(id);
+            //Category? categoryFromDb1 = _db.Categories.FirstOrDefault(u=> u.Id == id);
+            //Category? categoryFromDb2 = _db.Categories.Where(u=> u.Id == id).FirstOrDefault();
+            if (categoryFromDb == null) 
+            { 
+                return NotFound();
+            }
+
+            return View(categoryFromDb);
         }
 
         [HttpPost]
-        public IActionResult Edit(int? id)
+        public IActionResult Edit(Category obj)
         {
-            if (obj.Name == obj.DisplayOrder.ToString())
-            {
-                ModelState.AddModelError("name", "The Display Order cannot exactly match the Name");
-            }
+          
 
             if (ModelState.IsValid)
             {
-                _db.Categories.Add(obj);
+                _db.Categories.Update(obj);
                 _db.SaveChanges();
+                TempData["success"] = "Category updated successfully";
                 return RedirectToAction("Index");
             }
             return View();
 
         }
 
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Category? categoryFromDb = _db.Categories.Find(id);
+            //Category? categoryFromDb1 = _db.Categories.FirstOrDefault(u=> u.Id == id);
+            //Category? categoryFromDb2 = _db.Categories.Where(u=> u.Id == id).FirstOrDefault();
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+
+            return View(categoryFromDb);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeletePost(int? id)
+        {
+            Category? obj = _db.Categories.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            _db.Categories.Remove(obj);
+            _db.SaveChanges();
+            TempData["success"] = "Category Deleted successfully";
+            return RedirectToAction("Index");
+            
+
+        }
 
     }
 }
