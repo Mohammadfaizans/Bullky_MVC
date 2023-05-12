ae8a77914022875f7d1a47ffd5ed07eeaed8d8b6
Mohammadfaizan <m.faizan.s2255@gmail.com>
2023-05-13 01:22:00 +0530

    Added Category model and Controller with Action Methods

---
+++ BullkyWeb/Controllers/CategoryController.cs
@@ -0,0 +1,71 @@
+﻿using BullkyWeb.Data;
+using BullkyWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BullkyWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            var objCategoryList = _db.Categories.ToList();
+            return View(objCategoryList);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "The Display Order cannot exactly match the Name");
+            }
+
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+           return View();
+           
+        }
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Edit(int? id)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "The Display Order cannot exactly match the Name");
+            }
+
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+
+        }
+
+
+    }
+}
